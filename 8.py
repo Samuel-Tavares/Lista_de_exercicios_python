01e5871f41906aca6c4c269980f3b6e7a0937d21
Samuel <samutavares44@gmail.com>
2023-10-24 20:34:36 -0300

    Organizando repositório

--- 8.py
+++
@@ -1,9 +0,0 @@
-valor_hora = float(input('Informe o valor da sua hora: '))
-horas_trabalhadas = float(input('Horas trabalhadas no mês: '))
-
-salario = horas_trabalhadas * valor_hora
-
-print(
-    f'\nSeu salário de acordo com as horas trabalhadas foi: R${salario:.2f}')
-
-input('\nTecle "ENTER" para sair...')
--- 9.py
+++
@@ -1,8 +0,0 @@
-print('| CONVERSOR DE FAHRENHEIT PARA CELSIUS |')
-
-fahrenheit = float(input('Informe a temperatura em Fahrenheit: '))
-
-celsius = ((fahrenheit - 32) * 5 / 9)
-print('A temperatura em Celsius é: ' + str(celsius))
-
-input('Tecle "Enter" para sair...')
